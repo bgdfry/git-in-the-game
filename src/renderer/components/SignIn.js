0e5008574046b9a352b786379bacce3fc1fe51f0
kylem038 <misenckj@gmail.com>
2017-01-04 14:07:07 -0700

    Lock shows up, but app closes after finish

--- src/renderer/components/SignIn.js
+++ src/renderer/components/SignIn.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import AuthService from '../../utils/AuthService';
+
+const auth = new AuthService('Rr72oknlvqmkiLJLfczB6nqrFUZNzvPa', 'kylemisencik.auth0.com');
 
 export default function() {
   return (
@@ -6,7 +9,7 @@ export default function() {
       <h1>Git in the Game</h1>
       <button
         className="loginButton"
-        onClick={() => firebase.auth().signInWithPopup(provider)}
+        onClick={auth.login.bind(this)}
       >
         Sign In
       </button>
---
+++ src/utils/AuthService.js
@@ -0,0 +1,67 @@
+import { EventEmitter } from 'events';
+import { isTokenExpired } from './jwtHelper';
+import Auth0Lock from 'auth0-lock';
+
+export default class AuthService extends EventEmitter {
+  constructor(clientId, domain) {
+    super();
+
+    this.lock = new Auth0Lock(clientId, domain, {
+    auth: {
+      redirectUrl: `${window.location.origin}`,
+      responseType: 'token'
+    }
+  });
+
+    this.lock.on('authenticated', this._doAuthentication.bind(this));
+    this.login = this.login.bind(this);
+  }
+
+  _doAuthentication(authResult){
+    this.setToken(authResult.idToken);
+    this.lock.getProfile(authResult.idToken, (error, profile) => {
+      if (error) {
+        console.log('Error loading the Profile', error);
+      } else {
+        this.setProfile(profile);
+      }
+    });
+  }
+
+  login() {
+    this.lock.show();
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !isTokenExpired(token);
+  }
+
+  setProfile(profile) {
+    localStorage.setItem('profile', JSON.stringify(profile));
+    this.emit('profile_updated', profile);
+  }
+
+  getProfile() {
+    const profile = localStorage.getItem('profile');
+    return profile ? JSON.parse(localStorage.profile) : {};
+  }
+
+  setToken(idToken) {
+    localStorage.setItem('id_token', idToken);
+  }
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      var error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+  }
+}
