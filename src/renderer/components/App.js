38e95d8c3a56582e09db421148d4b06599f631b5
Blake Worsley <blakeworsley@gmail.com>
2017-01-04 15:51:34 -0700

    adds home page stub

--- src/renderer/components/App.js
+++ src/renderer/components/App.js
@@ -10,8 +10,23 @@ render(){
   return(
       <div>
         <h1>Connected</h1>
+        <div>
+          <h3>Mod 1 Repos:</h3>
+          <h2>22</h2>
+        </div>
+        <div>
+          <h3>Mod 1 Commits:</h3>
+          <h2>134</h2>
+        </div>
+        <div>
+          <h3>You VS Last Mod:</h3>
+          <h2>+22</h2>
+        </div>
+        <div>
+          <h4>14</h4>
+          <h4>Followers</h4>
+        </div>
       </div>
     );
   }
-
 };
