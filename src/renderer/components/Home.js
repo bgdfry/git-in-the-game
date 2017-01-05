a591d205851708cb4282b8b31b8d7d0ace84d651
Blake Worsley <blakeworsley@gmail.com>
2017-01-04 19:25:34 -0700

    adds stat class to home component

--- src/renderer/components/Home.js
+++ src/renderer/components/Home.js
@@ -11,18 +11,18 @@ render(){
       <div className='home-container'>
         <section className='home'>
           <section className='current-mod-stats'>
-            <div className='current-mod-repos'>
+            <div className='stat'>
               <h3>Mod 1 Repost:</h3>
               <h1>22</h1>
             </div>
-            <div className='current-mod-commits'>
+            <div className='stat'>
               <h3>Mod 1 Commits:</h3>
               <h1>134</h1>
             </div>
           </section>
 
 
-          <div className='current-mod-comparison'>
+          <div className='stat'>
             <h3>You VS Last Mod:</h3>
             <h1>+22</h1>
           </div>
