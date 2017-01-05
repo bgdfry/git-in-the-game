718cee0a255e25d05996f8d573f53bdc7f7ba8f2
kylem038 <misenckj@gmail.com>
2017-01-05 11:49:29 -0700

    Routing issue

--- src/components/NameInput.js
+++ src/components/NameInput.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default class NameInput extends React.Component {
-  constructor() {
-    super();
-}
-
-submitUserName() {
-
-}
-
-render(){
-  return(
-      <div className='home-container'>
-      <Link to={'/repos'} className='back back-left'>
-        <img src='./imgs/left-arrow.svg' />
-      </Link>
-        <section className='home'>
-          <input type='text' placeholder='Input GitHub username' value={this.props.value}></input>
-          <button type='submit' onClick={this.submitUserName()}>Submit</button>
-        </section>
-      </div>
-    );
-  }
+const NameInput = ({submitUserName}) => {
+return(
+    <div className='home-container'>
+    <Link to={'/repos'} className='back back-left'>
+      <img src='./imgs/left-arrow.svg' />
+    </Link>
+      <section className='home'>
+        <input type='text' placeholder='Input GitHub username' value=''></input>
+        <button type='submit' onClick={submitUserName(input.value)}>Submit</button>
+      </section>
+    </div>
+  );
 };
