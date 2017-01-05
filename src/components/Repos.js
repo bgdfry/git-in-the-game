3c428e9993893e68fc021f2b5e5920c21881c92b
Blake Worsley <blakeworsley@gmail.com>
2017-01-05 11:32:58 -0700

    adds repos with names and links

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,7 +4,27 @@ import { Link } from 'react-router';
 export default class Repos extends React.Component {
   constructor() {
     super();
-}
+    this.state = {
+      repositiories: [
+        { name: 'fish-wrangler', link: 'https://www.youtube.com/watch?v=xfr64zoBTAQ'},
+        { name: 'colorado-weather', link: 'https://www.youtube.com/watch?v=xfr64zoBTAQ'},
+        { name: 'jacked', link: 'https://www.youtube.com/watch?v=xfr64zoBTAQ'},
+        { name: 'social-influence', link: 'https://www.youtube.com/watch?v=xfr64zoBTAQ'}
+      ]
+    }
+  }
+  
+  renderRepoNames() {
+    return (
+      this.state.repositiories ? 
+        this.state.repositiories.map((repo) => {
+          return <a href={repo.link}><h3>{repo.name}</h3></a>
+        })
+          :
+        <h2>No Repos</h2>
+    )
+
+  }
 
 render(){
   return(
@@ -16,7 +36,8 @@ render(){
         <section className='home'>
           <section className='current-mod-stats'>
             <section className='stat'>
-
+              <h1>All Repos</h1>
+              {this.renderRepoNames()}
             </section>
             <form className='mod-form'>
               <section className='mod-input'>
