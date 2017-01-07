dc1ff0d3f61542f29e2f7357d8ac97e46e4ab1da
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-07 11:24:27 -0700

    Add userEvents container

---
+++ src/containers/userEvents.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { getEvents } from '../actions/index';
+
+const mapStateToProps = (state) => {
+  return {
+    events: state.events
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getEvents: (events) => {
+       dispatch(getEvents(events));
+     }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps);
