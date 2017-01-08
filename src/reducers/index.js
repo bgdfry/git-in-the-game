5cfc38d96d19c03825f0a8dfce134aa1231bbf33
Mike Sausa <mikejsausa@gmail.com>
2017-01-08 10:03:52 -0700

    combined opened pull requests reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import username from './username';
 import events from './event';
 import pushEvents from './pushEvents';
+import openedPullRequests from './openedPullRequests';
 
 export const rootReducer = combineReducers({
   username,
   events,
-  pushEvents
+  pushEvents,
+  openedPullRequests,
 });
 
 export default rootReducer;
