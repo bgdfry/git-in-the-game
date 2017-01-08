6196d6bcbc8be3d996e97bb938a38a3a64056170
Mike Sausa <mikejsausa@gmail.com>
2017-01-08 10:13:30 -0700

    combined issues created reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import username from './username';
 import events from './event';
 import pushEvents from './pushEvents';
 import openedPullRequests from './openedPullRequests';
+import issuesCreated from './issuesCreated';
 
 export const rootReducer = combineReducers({
   username,
   events,
   pushEvents,
   openedPullRequests,
+  issuesCreated,
 });
 
 export default rootReducer;
